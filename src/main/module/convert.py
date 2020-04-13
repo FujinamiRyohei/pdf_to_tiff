0a24ca16e36554a42a2926f52df1a7e3c8222caf
HigashidaTomoyuki <higashida.tomoyuki@tis.co.jp>
2020-04-13 18:30:35 +0900

    s3に保存するように変更

--- src/main/module/convert.py
+++ src/main/module/convert.py
@@ -1,6 +1,7 @@
-from pdf2image import convert_from_path
+from pdf2image import convert_from_bytes
 from logging import getLogger
 from .utils import Utils, LambdaRuntimeException
+from .s3_controller import S3Controller
 
 logger = getLogger(__name__)
 
@@ -8,39 +9,37 @@ logger = getLogger(__name__)
 class PDFtoTIFFConverter:
     def __init__(self):
         self._utils = Utils()
+        self._s3_controller = S3Controller()
 
     def execution(self, event, context):
         try:
             # 入力情報取得
             input_data = self._utils.json_parse(event)
-            content_type = self._utils.get_content_type_from_header(input_data["headers"])
-            input_filename = self._utils.get_input_filename_from_header(input_data["headers"])
-            output_file_extention = self._utils.get_output_file_extention_from_header(input_data["headers"])
-            input_file = input_data["body"]
+            body = self._utils.json_parse(input_data["body"])
+            input_key = body["input_key"]
 
-            # content-type確認
-            self._utils.validate_content_type(content_type)
-            
-            # 入力ファイル名確認
-            self._utils.validate_input_filename(input_filename)
+            # 入力キー確認
+            self._utils.validate_input_key_extention(input_key)
 
-            # 入力ファイル拡張子確認
-            self._utils.validate_input_file_extention(input_filename)
+            # S3からPDF取得
+            pdf_data = self._s3_controller.get_s3_object(input_key)
 
-            # 出力ファイル拡張子確認
-            self._utils.validate_output_file_extention(output_file_extention)
+            # 一時フォルダ作成
+            temp_dir = self._utils.make_temp_dir()
 
-            # 一時ファイル作成
-            self._utils.make_temp_dir()
-
-            # 一時ファイルにPDFを保存
-            input_file_path = self._utils.save_input_file(input_filename, input_file)
+            # 出力ファイル名作成
+            output_filename = self._utils.change_extention(input_key)
 
             # tifに変換
-            tif_data = self._convert(input_file_path)
-            
+            output_path = f"{temp_dir}{output_filename}"
+            tif_data = self._convert(pdf_data, output_path)
+
+            # s3に保存
+            output_key = self._s3_controller.put_s3_object(
+                tif_data, output_filename)
+
             # 成功応答
-            return self._utils.success_response(tif_data)
+            return self._utils.success_response(f"Save tif data {output_key}")
         except LambdaRuntimeException as e:
             # 想定内エラーをraise
             raise e
@@ -50,25 +49,24 @@ class PDFtoTIFFConverter:
             logger.exception(message)
             raise LambdaRuntimeException(503, message)
 
-    def _convert(self, input_file_path, output_format="TIFF", dpi=400, compression="group4"):
+    def _convert(self, pdf_data, output_path, output_format="TIFF", dpi=400, compression="group4"):
         try:
             # PDFからtifに変換
-            images = convert_from_path(input_file_path)
+            images = convert_from_bytes(pdf_data)
             # 白黒に変換
             images = [i.convert("1") for i in images]
             # マルチページTIFFに変換して保存
-            output_file_path = self._utils.change_extention(input_file_path)
             images[0].save(
-                output_file_path,
+                output_path,
                 format=output_format,
                 dpi=(dpi, dpi),
                 compression=compression,
                 save_all=True,
                 append_images=images[1:])
             # 変換したファイルを読み込み
-            with open(output_file_path, "rb") as f:
+            with open(output_path, "rb") as f:
                 output_file = f.read()
-            logger.info(f"Convert Success. Save {output_file_path}")
+            logger.info(f"Convert Success. Save {output_path}")
             return output_file
         except Exception:
             message = "Failed convert pdf to tiff"
--- src/main/module/utils.py
+++ src/main/module/utils.py
@@ -13,86 +13,41 @@ class Utils:
         self._temp_dir = "/tmp/data/"
         self._required_required_content_type = "application/pdf"
         self._required_input_extention = "pdf"
-        self._required_output_extention = "tif"
 
     def json_parse(self, json_string):
         if isinstance(json_string, str):
             return json.loads(json_string)
         return json_string
 
-    def get_content_type_from_header(self, headers):
-        if headers.get("Content-Type"):
-            return headers.get("Content-Type")
-        elif headers.get("content-type"):
-            return headers.get("content-type")
-    
-    def get_input_filename_from_header(self, headers):
-        if headers.get("InputFileName"):
-            return headers.get("InputFileName")
-        elif headers.get("inputfilename"):
-            return headers.get("inputfilename")
-
-    def get_output_file_extention_from_header(self, headers):
-        if headers.get("OutputFileExtention"):
-            return headers.get("OutputFileExtention")
-        elif headers.get("outputfileextention"):
-            return headers.get("outputfileextention")
-
-    def validate_content_type(self, content_type):
-        if content_type != self._required_required_content_type:
-            message = f"Content-Type is not {self._required_required_content_type}"
-            logger.warning(message)
-            raise LambdaRuntimeException(400, message)
-        logger.info(f"Content-Type is {content_type}")
-
-    def validate_input_filename(self, input_filename):
-        if "\\" in input_filename or "/" in input_filename:
-            message = "Invalid request parameter"
-            logger.warning(message)
-            raise LambdaRuntimeException(400, message)
-        logger.info(f"Input filename is {input_filename}")
-
-    def validate_input_file_extention(self, input_filename):
-        extention = Path(input_filename).suffix
+    def validate_input_key_extention(self, input_key):
+        extention = Path(input_key).suffix
         if extention != f".{self._required_input_extention}":
-            message = f"Input file extention is not {self._required_input_extention}"
+            message = f"Input key extention is not {self._required_input_extention}"
             logger.warning(message)
             raise LambdaRuntimeException(400, message)
-        logger.info(f"Input file extention is {extention}")
-
-    def validate_output_file_extention(self, output_extention):
-        if output_extention != self._required_output_extention:
-            message = f"Output file extention is not {self._required_output_extention}"
-            logger.warning(message)
-            raise LambdaRuntimeException(400, message)
-        logger.info(f"Output file extention is {output_extention}")
+        logger.info(f"Input key extention is {extention}")
 
     def make_temp_dir(self):
         if os.path.exists(self._temp_dir):
             shutil.rmtree(self._temp_dir)
         os.makedirs(self._temp_dir)
         logger.info(f"Make directory {self._temp_dir}")
+        return self._temp_dir
 
-    def save_input_file(self, input_filename, input_file):
-        save_file_path = f"{self._temp_dir}{input_filename}"
-        with open(save_file_path, "wb") as f:
-            f.write(base64.b64decode(input_file.encode("utf8")))
-        logger.info(f"Save input file {save_file_path}")
-        return save_file_path
+    def change_extention(self, input_key, extention="tif"):
+        output_filename = Path(input_key).with_suffix(f'.{extention}').name
+        logger.info(f"Output filename is {output_filename}")
+        return output_filename
 
-    def change_extention(self, input_file_path, extention="tif"):
-        output_file_path = Path(input_file_path).with_suffix(f'.{extention}')
-        logger.info(f"Output file path is {output_file_path}")
-        return str(output_file_path)
-
-    def success_response(self, output_file, code=200, content_type='application/pdf', encode=True):
+    def success_response(self, message, code=200, content_type='application/pdf'):
         response = {
             'statusCode': code,
             'headers': {
                 'content-type': content_type
             },
-            "isBase64Encoded": encode,
-            'body': base64.b64encode(output_file)
+            'body': {
+                "message": message
+            }
         }
         return response
 
